6776e7b762c35ce70a775bedf13510bb3fe690b9
gazolukas <43939608+gazolukas@users.noreply.github.com>
2022-05-19 16:02:30 +0200

    feat(chart): Create chart (#1)

---
+++ src/client/utils/chart.ts
@@ -0,0 +1,38 @@
+import moment, { Moment } from 'moment';
+
+import { UserApiType, UserType } from '../types/users';
+
+export const filter = (data: UserApiType[], from?: Moment, to?: Moment): UserType[] => {
+  const result: UserType[] = [];
+
+  data
+    .filter(({ birthday }) => {
+      // Filter by date
+      if (from && to) {
+        return moment(birthday).isBetween(from, to, 'day', '[]');
+      }
+      return true;
+    })
+    .forEach((person) => {
+      // Get number of people for specific country
+      if (
+        result.some((val) => {
+          return val.country === person.country;
+        })
+      ) {
+        result.forEach((val) => {
+          if (val.country === person.country) {
+            val.y++;
+          }
+        });
+      } else {
+        const obj = {} as UserType;
+
+        obj.country = person.country;
+        obj.y = 1;
+        result.push(obj);
+      }
+    });
+
+  return result;
+};
